b1c55cdc50c496f450514a81a74558c793cc153b
Roberto Neglia <roberto.neglia@mail.polimi.it>
2023-02-15 18:38:49 +0100

    python comparer tolerance fix

--- Poisson2D/cmp.py
+++ Poisson2D/cmp.py
@@ -6,14 +6,17 @@ if(len(sys.argv) > 3):
     print('Too many arguments')
     print('Correct usage: python cmp.py file1.csv file2.csv')
     exit()
-if(len(sys.argv)<3):
+if(len(sys.argv) < 3):
     print('Too few arguments')
     print('Correct usage: python cmp.py file1.csv file2.csv')
     exit()
+
+tol = 1.e-5
+
 sol1 = pd.read_csv(sys.argv[1], sep=",")
 sol2 = pd.read_csv(sys.argv[2], sep=",")
 
-if(all(sol1.v == sol2.v)):
+if(all(abs(sol1.v-sol2.v) < tol)):
     print('THE TWO SOLUTIONS ARE THE SAME!')
 else:
     print('NOOOOOOOO THE TWO SOLUTIONS ARE NOT THE SAME!')
